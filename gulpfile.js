c59a0ea8bb87257ad9512a77f412650a8a8dbabf
Федор Шадрин <shadowfieng@gmail.com>
2019-11-27 00:02:42 +0500

    gulp fixed, started products section

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,8 @@ gulp.task('style', function() {
 gulp.task('script', function() {
     return gulp.src([
             'node_modules/slick-carousel/slick/slick.js',
-            'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+            'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+            'node_modules/mixitup/dist/mixitup.js'
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -60,7 +61,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('js'))
 
